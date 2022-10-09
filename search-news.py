51bae711d6b25cc706223732edd7686c190f87f4
ruolin22 <ruolin.chua.2019@sis.smu.edu.sg>
2022-10-09 23:33:59 +0800

    Update search-news.py
    loop through list of individuals in CSV file and create CSV of scraped results for each individual

--- search-news.py
+++ search-news.py
@@ -1,75 +1,84 @@
 #import what we need
 from requests_html import HTMLSession
 import time
+import pandas as pd
 
-session = HTMLSession()
 
-#use session to get the page
-#Duckduckgo
-#r = session.get('https://duckduckgo.com/?q=robert+downey+jr&atb=v314-1&iar=news&ia=news')
+# list of individuals we are querying
+search_list = pd.read_csv("search-list.csv")
 
-#Google
-r = session.get('https://www.google.com/search?q=robert+downey+jr&sxsrf=ALiCzsYx3PgO2Q0fFe0Al_LplnUUVeU9uQ:1664606729446&source=lnms&tbm=nws&sa=X&ved=2ahUKEwiylLKmt776AhW0R2wGHUh7AdUQ_AUoAnoECAIQBA&biw=1918&bih=943&dpr=1')
+for x in range(len(search_list.index)):
 
-#Rendering of page
-r.html.render(sleep=1, scrolldown=2)
+    #use session to get the page
+    #Duckduckgo
+    #r = session.get('https://duckduckgo.com/?q=robert+downey+jr&atb=v314-1&iar=news&ia=news')
 
-articles = r.html.find('a.WlydOe')
+    #Google
+    session = HTMLSession()
 
-#list of result
-newslist = []
+    #retrieve URL link for each individual
+    r = session.get(search_list["URL"][x])
 
-#number of pages to scrap
-numberOfPage = 2
-
-for i in range(numberOfPage):
-    
-    for item in articles:
-    ##    #Duck duck go
-    ##    try:
-    ##        newsitem = item.find('h2', first=True)
-    ##        title = newsitem.text
-    ##        link = newsitem.absolute_links
-    ##        articleTime = item.find('.result__timestamp', first=True).text
-    ##        description = item.find('.result__snippet', first=True).text
-    ##        newsarticle = {
-    ##            'title': title,
-    ##            'link': link,
-    ##            'time': articleTime,
-    ##            'description' : description
-    ##        }
-    ##        newslist.append(newsarticle)
-    ##    except:
-    ##       pass
-        
-        # Google
-        try:
-            newsitem = item.find('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d', first=True)
-            title = newsitem.text
-            articleTime = item.find('.OSrXXb', first=True).text
-            description = item.find('.GI74Re', first=True).text
-            newsarticle = {
-                'title': title,
-                'time': articleTime,
-                'description' : description
-            }
-            newslist.append(newsarticle)
-        except:
-           pass
-
-    #Proceeding to next page
-    nextPage = r.html.find('.d6cvqb.BBwThe')[-1]
-    nextPageURL = nextPage.find('a', first=True).attrs['href']
-    nextPageURL = 'https://www.google.com' + nextPageURL
-    r = session.get(nextPageURL)
-
-    #Render and sleep to prevent ban
+    #Rendering of page
     r.html.render(sleep=1, scrolldown=2)
+
     articles = r.html.find('a.WlydOe')
-    time.sleep(2)
 
-#ends process
-session.close()
+    #list of result
+    newslist = []
 
-#print the length of the list
-print(newslist)
+    #number of pages to scrap
+    numberOfPage = 5
+
+    for i in range(numberOfPage):
+        
+        for item in articles:
+        ##    #Duck duck go
+        ##    try:
+        ##        newsitem = item.find('h2', first=True)
+        ##        title = newsitem.text
+        ##        link = newsitem.absolute_links
+        ##        articleTime = item.find('.result__timestamp', first=True).text
+        ##        description = item.find('.result__snippet', first=True).text
+        ##        newsarticle = {
+        ##            'title': title,
+        ##            'link': link,
+        ##            'time': articleTime,
+        ##            'description' : description
+        ##        }
+        ##        newslist.append(newsarticle)
+        ##    except:
+        ##       pass
+            
+            # Google
+            try:
+                newsitem = item.find('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d', first=True)
+                title = newsitem.text
+                articleTime = item.find('.OSrXXb', first=True).text
+                description = item.find('.GI74Re', first=True).text
+                newsarticle = {
+                    'title': title,
+                    'time': articleTime,
+                    'description' : description
+                }
+                newslist.append(newsarticle)
+            except:
+                pass
+
+        #Proceeding to next page
+        nextPage = r.html.find('.d6cvqb.BBwThe')[-1]
+        nextPageURL = nextPage.find('a', first=True).attrs['href']
+        nextPageURL = 'https://www.google.com' + nextPageURL
+        r = session.get(nextPageURL)
+
+        #Render and sleep to prevent ban
+        r.html.render(sleep=1, scrolldown=2)
+        articles = r.html.find('a.WlydOe')
+        time.sleep(2)
+
+    #ends process
+    session.close()
+
+    df = pd.DataFrame(newslist)
+    df.to_csv(search_list["Name"][x]+'.csv')
+
